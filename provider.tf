21928a18611b4389e83f4f691201c89bccfa8584
spstorage <104639750+spstorage@users.noreply.github.com>
2025-08-01 22:16:54 +0530

    Update provider.tf
    aws provider

--- provider.tf
+++ provider.tf
@@ -1,1 +1,12 @@
-
+terraform {
+  required_providers {
+    aws={
+      source= "hashicorp/aws"
+      version = "~> 5.40"  # Use latest stable 5.x version
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+provider "aws" {
+  region = "ap-south-1"
+}
